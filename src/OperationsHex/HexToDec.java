976baf841c281f7b0097bf894f95d026baeef49b
Jefferson <jefferson.bscomp@gmail.com>
2021-10-01 20:52:43 -0300

    Classe com a operação HexToDec

--- src/OperationsHex/HexToDec.java
+++ src/OperationsHex/HexToDec.java
@@ -1,4 +1,29 @@
 package OperationsHex;
 
 public class HexToDec {
+    String imput;
+
+    public String getImput() {
+        return imput;
+    }
+
+    public void setImput(String imput) {
+        this.imput = imput;
+    }
+
+    public int converter(String hex) {
+        String digits = "0123456789ABCDEF";
+        hex = hex.toUpperCase();
+        int value = 0;
+
+        for (int i = 0; i < hex.length(); i++)
+        {
+            char c = hex.charAt(i);
+            int d = digits.indexOf(c);
+            value = 16 * value + d;
+        }
+        return value;
+    }
+
+
 }
