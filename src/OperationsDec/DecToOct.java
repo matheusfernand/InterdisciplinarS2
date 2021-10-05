ec5a118ec015dc8f55951aa51888cad4e7dc74f1
Jefferson <jefferson.bscomp@gmail.com>
2021-10-04 23:40:57 -0300

    Classe operação DecToOct

--- src/OperationsDec/DecToOct.java
+++ src/OperationsDec/DecToOct.java
@@ -1,4 +1,28 @@
 package OperationsDec;
 
 public class DecToOct {
+
+    private int index;
+
+    public int getIndex(int i) {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    public int conv()
+    {
+        int octalNumber = 0, i = 1;
+
+        while (this.index != 0)
+        {
+            octalNumber += (this.index % 8) * i;
+            this.index /= 8;
+            i *= 10;
+        }
+
+        return octalNumber;
+
+    }
 }
