ac8434d2b94b3e915bf01dba4e313e75c7f16a0e
MatheusWAlvarenga <matheuswalvarenga@gmail.com>
2021-11-27 19:36:45 -0300

    Matheus Alvarenga- Dealing with input errors

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,90 +1,150 @@
 package Application;
 
-import OperationsBin.BinToDec;
-import OperationsDec.DecToOct;
-import OperationsHex.HexToDec;
-import OperationsOct.OctToDec;
-
 import java.util.Scanner;
 
 public class Main {
 
+
+
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		String number;
-		int numberI;
-		int baseOut;
-		int baseInput;
+		int number = 0;
+		String numberHexa;
+		int baseOut = 0;
+		int baseInput = 0;
 		float result;
 
 		// Menu selection input base.
 		System.out.print("\n 1 Decimal \n 2 Octal \n 3 Binário \n 4 Hexadecimal\nEscolha a base de entrada: ");
-		baseInput =  input.nextInt();
+
+		// Input data verification
+		try {
+			baseInput =  input.nextInt();
+			boolean verify = Integer.toString(baseInput).matches("-?[1-4]{1,1}+");
+			if (verify == false){
+				throw new Exception();
+			}
+		} catch (Exception err) {
+			System.out.print("Opção inválida");
+			System.exit(0);
+		}
 
 		// To input Decimal
 		if (baseInput == 1) {
 			System.out.print("\nDigite um número Decimal: ");
-			numberI = input.nextInt();
 
-			//Input data verification
+			// Input data verification
+			try {
+				number = input.nextInt();
+				boolean verify = Integer.toString(number).matches("-?[0-9]{1,}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Não foi inserido um número na base Decimal");
+				System.exit(0);
+			}
 
 			// Menu selection output base.
 			System.out.print("\n 1 Octal \n 2 Binário \n 3 Hexadecimal \nEscolha a base de saída: ");
-			baseOut = input.nextInt();
 
-			// Output verification
+			// Option Output verification
+			try {
+				baseOut = input.nextInt();
+				boolean verify = Integer.toString(baseOut).matches("-?[1-3]{1,1}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Opção inválida");
+				System.exit(0);
+			}
+
 			if(baseOut == 1) {
-				DecToOct decToOct = new DecToOct();
-				decToOct.setEntradaDecToOct(numberI);
-				System.out.print("\nO número decimal "+numberI+" convertido em octal é: "+decToOct.conversionDecOct());
+				System.out.print("\nO número decimal "+number+" convertido em octal é: \n" );
 			} else if(baseOut == 2) {
-				System.out.print("\nO número decimal "+numberI+" convertido em binário é: \n" );
+				System.out.print("\nO número decimal "+number+" convertido em binário é: \n" );
 			} else if(baseOut == 3) {
-				System.out.print("\nO número decimal "+numberI+" convertido em hexadecimal é: \n" );
+				System.out.print("\nO número decimal "+number+" convertido em hexadecimal é: \n" );
 			}
 		}
 		// To input Octal
 		else if (baseInput == 2) {
 			System.out.print("\nDigite um número octal: ");
-			numberI = input.nextInt();
 
-			//Input data verification
+			// Input data verification
+			try {
+				number = input.nextInt();
+				boolean verify = Integer.toString(number).matches("-?[0-7]{1,}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Não foi inserido um número na base Octal");
+				System.exit(0);
+			}
 
 			// Menu selection output base.
 			System.out.print("\n 1 Decimal \n 2 Binário \n 3 Hexadecimal\nEscolha a base de saída: ");
-			baseOut = input.nextInt();
 
-			// Output verification
+			// Option Output verification
+			try {
+				baseOut = input.nextInt();
+				boolean verify = Integer.toString(baseOut).matches("-?[1-3]{1,1}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Opção inválida");
+				System.exit(0);
+			}
+
 			if(baseOut == 1) {
-				OctToDec octToDec = new OctToDec();
-				octToDec.setEntradaOctToDec(numberI);
-
-				System.out.print("\nO número octal "+numberI+" convertido em decimal é: " + octToDec.conversionOctDec(numberI));
+				System.out.print("\nO número octal "+number+" convertido em decimal é: \n" );
 			} else if(baseOut == 2) {
-				System.out.print("\nO número octal "+numberI+" convertido em binário é: \n" );
+				System.out.print("\nO número octal "+number+" convertido em binário é: \n" );
 			} else if(baseOut == 3) {
-				System.out.print("\nO número octal "+numberI+" convertido em hexadecimal é: \n" );
+				System.out.print("\nO número octal "+number+" convertido em hexadecimal é: \n" );
 			}
 		}
 		// To input Binário
 		else if (baseInput == 3) {
 			System.out.print("\nDigite um número binário: ");
-			number = input.next();
 
-			//Input data verification
+			// Input data verification
+			try {
+				number = input.nextInt();
+				boolean verify = Integer.toString(number).matches("-?[0-1]{1,}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Não foi inserido um número na base Binária");
+				System.exit(0);
+			}
 
 			// Menu selection output base.
 			System.out.print("\n 1 Octal \n 2 Decimal \n 3 Hexadecimal\nEscolha a base de saída: ");
-			baseOut = input.nextInt();
 
-			// Output verification
+			// Option Output verification
+			try {
+				baseOut = input.nextInt();
+				boolean verify = Integer.toString(baseOut).matches("-?[1-3]{1,1}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Opção inválida");
+				System.exit(0);
+			}
+
 			if(baseOut == 1) {
-				System.out.print("\nO número binário "+number+" convertido em Octal é:" );
+				System.out.print("\nO número binário "+number+" convertido em Octal é: \n" );
 			} else if(baseOut == 2) {
-				BinToDec binToDec = new BinToDec();
-				binToDec.setEntradaBinDec(number);
-				System.out.print("\nO número binário "+number+" convertido em Decimal é:" + BinToDec.conversionBinDec(number));
+//				int numberw = Integer.getInteger(number);
+//				BinToDec tested = new BinToDec(numberw);
+//				tested.calculadorCientifica(tested);
 			} else if(baseOut == 3) {
 				System.out.print("\nO número binário "+number+" convertido em hexadecimal é: \n" );
 			}
@@ -92,25 +152,42 @@ public class Main {
 		// To input Hexadecimal
 		else if (baseInput == 4) {
 			System.out.print("\nDigite um número hexadecimal: ");
-			number = input.next();
 
-			//Input data verification
+			// Input data verification
+			try {
+				numberHexa = input.next();
+				boolean verify = numberHexa.matches("-?[0-9a-fA-F]+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Não foi inserido um número na base Hexadecimal");
+				System.exit(0);
+			}
 
 			// Menu selection output base.
 			System.out.print("\n 1 Octal \n 2 Decimal \n 3 Binário\nEscolha a base de saída: ");
-			baseOut = input.nextInt();
 
-			// Output verification
+			// Option Output verification
+			try {
+				baseOut = input.nextInt();
+				boolean verify = Integer.toString(baseOut).matches("-?[1-3]{1,1}+");
+				if (verify == false){
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.print("Opção inválida");
+				System.exit(0);
+			}
+
 			if(baseOut == 1) {
 				System.out.print("\nO número hexadecimal "+number+" convertido em Octal é: \n" );
 			} else if(baseOut == 2) {
-				HexToDec hexToDec = new HexToDec();
-				hexToDec.setEntradaHexToDec(number);
-				System.out.print("\nO número hexadecimal " + number + " convertido em Decimal é: " + hexToDec.conversionHexDec(number));
+				System.out.print("\nO número hexadecimal "+number+" convertido em Decimal é: \n" );
 			} else if(baseOut == 3) {
 				System.out.print("\nO número hexadecimal "+number+" convertido em hexadecimal é: \n" );
 			}
 		}
 	}
-	}
+}
 
